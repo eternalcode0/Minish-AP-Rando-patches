0aff1c63f7e8bb7cfc92856f5429fba3b3682c17
EternalCode <eternalcode0@gmail.com>
2025-07-02 12:16:25 -0400

    new save creation loads from rom

--- src/asm/newGame.s
+++ src/asm/newGame.s
@@ -1,35 +1,29 @@
-.equ	return, newGameTable+4
+.equ	savesize, savereg+4
+.equ	savestart, savesize+4
+.equ	safereturn, savestart+4
 .thumb
 push	{r0-r7}
-
-ldr	r4,newGameTable
-bigloop:
-ldr	r5,[r4]
-cmp	r5,#0
-beq	End
-ldr	r6,[r4,#4]
-ldr	r7,[r4,#8]
-smallloop:
-cmp	r7,#0
-beq	nextbigloop
-ldrb	r0,[r5]
-strb	r0,[r6]
-sub	r7,#1
-add	r5,#1
-add	r6,#1
-b	smallloop
-nextbigloop:
-add	r4,#12
-b	bigloop
-
-End:
-ldr	r3,return
-mov	lr,r3
+ldr	r4, savereg
+ldr	r5, savesize
+ldr	r6, savestart
+loop:
+cmp	r5, #0
+beq	end
+ldrb	r0, [r4, r5]
+cmp	r0, #0
+beq	skipbyte
+strb	r0, [r6, r5]
+skipbyte:
+sub	r5, #1
+b	loop
+end:
+ldr	r3, safereturn
+mov	lr, r3
 pop	{r0-r7}
 .short	0xF800
 
 .align
 .ltorg
-newGameTable:
-@POIN newGameTable
-@POIN return
+savereg:
+@POIN savesize
+@POIN savestart
